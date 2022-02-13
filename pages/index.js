c3927e41983136a9124bcc27911cf7ad270947d1
thinhnp13 <thinhnp13.fsoft@gmail.com>
2022-02-13 16:38:39 +0700

    add css boostrap andt global

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 const Index = () => {
   return (
     <>
-      <h1>Hello From Next</h1>
+      <h1 className='jumbotron text-center bg-primary'>Hello World</h1>
+      <h2>From NextJS</h2>
     </>
   )
 }
